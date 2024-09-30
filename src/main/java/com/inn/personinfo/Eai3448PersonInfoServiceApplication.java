8b3cdfae30839f43481e0fb259775704375438e8
ganeshdoke001 <ganeshdoke1999@gmail.com>
2024-09-30 09:54:51 +0530

    v1:3448, created person info project

---
+++ src/main/java/com/inn/personinfo/Eai3448PersonInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inn.personinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eai3448PersonInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eai3448PersonInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/personinfo/Eai3448PersonInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.personinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai3448PersonInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
